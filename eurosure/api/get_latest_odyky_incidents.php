7cc925b5fd4096029535c9c9367e9186af162fc3
Michalis Ermogenous <m.ermogenous@eurosure.lan>
2021-02-25 19:19:14 +0200

    eurosure odyky apis

--- eurosure/api/get_latest_odyky_incidents.php
+++ eurosure/api/get_latest_odyky_incidents.php
@@ -36,23 +36,43 @@ if ($extranet -> connect_errno) {
 $lastIncidentID = $db->get_setting('eurosure_last_odyky_incident');
 
 //now find on extranet any new incidents where the primary key is higher
-$sql = 'SELECT * FROM es_odyky_incidents WHERE esoin_incident_id > '.$lastIncidentID;
+//$sql = 'SELECT * FROM es_odyky_incidents WHERE esoin_incident_id > '.$lastIncidentID;
+$sql = 'SELECT * FROM es_odyky_incidents WHERE esoin_processed = 0';
+
 $result = $extranet->query($sql);
 $lastID = 0;
 while ($row = $result->fetch_assoc()){
     $lastID = $row['esoin_incident_id'];
     //connect to odyky api to get the information and files for this incident
+    getOdykyFiles($row['esoin_odyky_incident_id']);
 
-
+    //update extranet that this record has been processed
+    $sql = 'UPDATE es_odyky_incidents SET esoin_processed = 1 WHERE esoin_incident_id = '.$row['esoin_incident_id'];
+    //$extranet->query($sql);
 
 }
 
 //update settings
 if ($lastID > $lastIncidentID){
-    $db->update_setting('eurosure_last_odyky_incident',$lastID);
+    //temp disable
+    //$db->update_setting('eurosure_last_odyky_incident',$lastID);
 }
 
 
 function getOdykyFiles($odykyID){
+
+    $ch = curl_init();
+// IMPORTANT: the below line is a security risk, read https://paragonie.com/blog/2017/10/certainty-automated-cacert-pem-management-for-php-software
+// in most cases, you should set it to true
+    $url = 'https://portal.odyky.com/webservices/eurosure/search_documents_by_id.json?user=eurosure&pass=Tt04U17paE7WSJvNwFFz&incident='.$odykyID;
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_URL, $url);
+    $result = curl_exec($ch);
+    curl_close($ch);
+
+    $obj = json_decode($result);
+    //echo $obj->access_token;
+    print_r($obj);
 
 }
--- eurosure/api/vehicle_details_api.php
+++ eurosure/api/vehicle_details_api.php
@@ -14,17 +14,33 @@ $db->apiGetReadHeaders();
 
 if ($_SERVER['REMOTE_ADDR'] != '213.207.149.26'
     && $_SERVER['REMOTE_ADDR'] != '82.102.46.7' /*Ali House*/
-    && $_SERVER['REMOTE_ADDR'] != '5.79.78.235' /*Odyky IP */){
+    && $_SERVER['REMOTE_ADDR'] != '5.79.78.235' /*Odyky IP */) {
 
     header("Location: http://www.eurosure.net");
     exit();
 }
 
+//also include id or policy number
 
+if ($_GET['usr'] == 'odyky_usr' && $_GET['psw'] == 'odod-21_aa(36^') {
 
-if ($_GET['reg'] != '' && $_GET['usr'] == 'odyky_usr' && $_GET['psw'] == 'odod-21_aa(36^'){
+    if ($_GET['reg'] == '' && $_GET['identity'] == '' && $_GET['policy'] == '') {
+        exit();
+    }
+    else {
+        $where = '';
+        if ($_GET['reg'] != '') {
+            $where = "esrsc_registration LIKE '%" . $_GET['reg'] . "%'";
+        } else if ($_GET['identity'] != '') {
+            $where = "esrsc_client_id LIKE '%" . $_GET['identity'] . "%'";
+        } else if ($_GET['policy'] != '') {
+            $where = "esrsc_policy_number LIKE '%" . $_GET['policy'] . "%'";
+        }
+        if ($where == '') {
+            exit();
+        }
 
-    $sql = "
+        $sql = "
         SELECT 
         esrsc_policy_number as policy_number,
         esrsc_client_id as client_id,
@@ -46,26 +62,25 @@ if ($_GET['reg'] != '' && $_GET['usr'] == 'odyky_usr' && $_GET['psw'] == 'odod-2
         FROM
              es_rescueline_vehicles
         WHERE
-        esrsc_registration LIKE '%".$_GET['reg']."%'
+        " . $where . "
+        ORDER BY esrsc_policy_number ASC
         ";
-    if ($result = mysqli_query($db->db_handle,$sql)) {
-        while ($data = $db->fetch_assoc($result)) {
-            //$data['home_city'] = mb_convert_encoding($data['home_city'], "ISO-8859-1", "UTF-8");
-            $output[] = $data;
+        if ($result = mysqli_query($db->db_handle, $sql)) {
+            while ($data = $db->fetch_assoc($result)) {
+                //$data['home_city'] = mb_convert_encoding($data['home_city'], "ISO-8859-1", "UTF-8");
+                $output[] = $data;
+            }
+
+            echo json_encode($output);
+            exit();
+        } else {
+            echo $sql . PHP_EOL . PHP_EOL;
+            echo mysqli_error($db->db_handle);
+            echo "<hr>";
+            echo mysqli_errno($db->db_handle);
+            $db->update_log_file_custom($sql, 'Rescueline API get vehicle details ERROR');
         }
-
-        echo json_encode($output);
-        exit();
     }
-    else {
-        echo $sql.PHP_EOL.PHP_EOL;
-        echo mysqli_error($db->db_handle);
-        echo "<hr>";
-        echo mysqli_errno($db->db_handle);
-        $db->update_log_file_custom($sql,'Rescueline API get vehicle details ERROR');
-    }
-
-}
-else {
+} else {
     exit();
 }
